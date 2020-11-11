b78f5259c7e3bb64ba130030d140bfebe2646ec1
jredfox <jredfox@hotmail.com>
2020-11-11 10:31:21 -0600

    0.2.0-stable
    last stable version before changing everything

--- src/jredfox/filededuper/Main.java
+++ src/jredfox/filededuper/Main.java
@@ -18,7 +18,7 @@ import com.google.common.io.Files;
 public class Main {
 	
 	public static boolean errored;
-	public static final String VERSION = "0.2";
+	public static final String VERSION = "0.2.0";
 	
 	public static void main(String[] args)
 	{
